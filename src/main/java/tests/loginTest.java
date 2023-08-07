cf2e2a792654e91620a2fb5a835a2b5bfc27e910
RtrojanNukeOgre <arimiyahmubarak@gmail.com>
2023-08-07 12:25:25 +0100

    LoginTest added

--- src/main/java/tests/loginTest.java
+++ src/main/java/tests/loginTest.java
@@ -1,14 +1,33 @@
 package tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import baseContainer.TestBase;
+import pages.loginPage;
 
 public class loginTest extends TestBase{
-	
+	loginPage actn = new loginPage(driver);
 	@Test
-	public void inputValue() {
-				
+	public void validLogin(String email, String password) throws Exception {
+		actn.inputEmail(email);
+		actn.inputPassword(password);
+		actn.clickLogin();
 	}
+	
+	public void navigateToSignUp() {
+		loginPage actn = new loginPage(driver);
+		actn.NaviatetoSignUp();
+	}
+	
+	@DataProvider(name = "LoginData")
+    public Object[][] testData() throws Exception
+    {
+        return new Object[][]
+                {
+        				{"email", "password"},
+        				
+                };
+    }
 
 }
